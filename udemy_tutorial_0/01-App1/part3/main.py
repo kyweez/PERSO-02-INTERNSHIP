0a628d93d9615fe30f20e67e611b6d5266be924a
kyweez <auboudier.pro@gmail.com>
2021-03-19 14:18:59 +0100

    udemy/app1/part3-done

--- udemy_tutorial_0/01-App1/part3/main.py
+++ udemy_tutorial_0/01-App1/part3/main.py
@@ -2,16 +2,34 @@ from folium import Map, Popup
 from geo import Geopoint
 
 # Get latitude and longitude values
-latitude = 45.764043
-longitude = 4.835659 
+latitude = 45.766043
+longitude = 4.860659 
 
 # Folium Map instance
 my_map = Map(location = [latitude, longitude])
 
-# Create a Geopoint instance
+# Creating a geopoint
 geopoint = Geopoint(latitude, longitude)
-popup = Popup(str(geopoint.get_weather()))
+
+# Getting the geopoint forecast
+forecast = geopoint.get_weather()
+
+# Generating the popup content
+popup_content = f"""
+{forecast[0][0][-8:-6]}h / {round(((forecast[0][1]-32) * 5/9), 1)}°C <img src="http://openweathermap.org/img/wn/{forecast[0][-1]}@2x.png" width="35">
+<hr style="margin:1px">
+{forecast[1][0][-8:-6]}h / {round(((forecast[1][1]-32) * 5/9), 1)}°C <img src="http://openweathermap.org/img/wn/{forecast[1][-1]}@2x.png" width="35">
+<hr style="margin:1px">
+{forecast[2][0][-8:-6]}h / {round(((forecast[2][1]-32) * 5/9), 1)}°C <img src="http://openweathermap.org/img/wn/{forecast[2][-1]}@2x.png" width="35">
+<hr style="margin:1px">
+{forecast[3][0][-8:-6]}h / {round(((forecast[3][1]-32) * 5/9), 1)}°C <img src="http://openweathermap.org/img/wn/{forecast[3][-1]}@2x.png" width="35">
+"""
+
+# Creating and adding popup to geopoint
+popup = Popup(popup_content, max_width=400)
 popup.add_to(geopoint)
+
+# Adding geopoint to the map
 geopoint.add_to(my_map)
 
 # Save the Map instance into HTML file
