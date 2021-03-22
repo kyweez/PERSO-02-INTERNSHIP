4791d8543014a11615c20e941ff295c0c94dbf23
kyweez <auboudier.pro@gmail.com>
2021-03-22 13:41:25 +0100

    udemy/deleting-venv

--- udemy_tutorial_0/02-App2/src/main.py
+++ udemy_tutorial_0/02-App2/src/main.py
@@ -1,3 +1,4 @@
+from pdfReport import PdfReport
 from flatmate import Flatmate
 from bill import Bill
 
@@ -7,3 +8,6 @@ flatmate_mary = Flatmate("Mary", 25)
 
 print("John pays", flatmate_john.pays(the_bill, flatmate_mary))
 print("Mary pays", flatmate_mary.pays(the_bill, flatmate_john))
+
+pdf_report = PdfReport("Report.pdf")
+pdf_report.generate(flatmate_john, flatmate_mary, the_bill)
--- udemy_tutorial_0/02-App2/src/pdfReport.py
+++ udemy_tutorial_0/02-App2/src/pdfReport.py
@@ -1,4 +1,4 @@
-from
+from fpdf import FPDF
 
 class PdfReport:
 
@@ -12,5 +12,34 @@ class PdfReport:
     def __init__(self, filename):
         self.filename = filename
 
-    def generate(self, flatemate1, flatmate2, bill):
-        pass
+    def generate(self, flatmate1, flatmate2, bill):
+        flatmate1_pay = f"{round(flatmate1.pays(bill, flatmate2), 2)}"
+        flatmate2_pay = f"{round(flatmate2.pays(bill, flatmate1), 2)}"
+
+        # Instanciate the pdf object
+        pdf = FPDF(orientation="P", unit="pt", format="A4")
+
+        # Creating a new page
+        pdf.add_page()
+
+        # Adding image
+        pdf.image(link="https://www.superior-alarm.com/wp-content/uploads/2016/11/house-png-202.png")
+
+        # Insert title
+        pdf.set_font(family="Times", size=24, style="B")
+        pdf.cell(w=0, h=80, txt="Flatmate Bill", border=1, align="C", ln=1)
+
+        # Insert period label and value
+        pdf.cell(w=100, h=40, txt="Period :", border=1)
+        pdf.cell(w=150, h=40, txt=bill.period , border=1, ln=1)
+
+        # Insert name and due amount of flatmate 1
+        pdf.cell(w=150, h=40, txt=flatmate1.name, border=1)
+        pdf.cell(w=150, h=40, txt=flatmate1_pay, border=1, ln=1)
+
+        # Insert name and due amount of flatmate 2
+        pdf.cell(w=150, h=40, txt=flatmate2.name , border=1)
+        pdf.cell(w=150, h=40, txt=flatmate2_pay, border=1, ln=1)
+
+        # Generate the PDF file
+        pdf.output(self.filename)
