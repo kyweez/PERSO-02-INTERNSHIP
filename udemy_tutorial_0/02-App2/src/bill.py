bf0c631d6800e6380a30b9914aee2f4b5a63428b
kyweez <auboudier.pro@gmail.com>
2021-03-22 15:33:20 +0100

    udemy/app2/cleaning-code

--- udemy_tutorial_0/02-App2/src/bill.py
+++ udemy_tutorial_0/02-App2/src/bill.py
@@ -1,8 +1,8 @@
 class Bill:
+    
     """
     Objects that contains data about a bill, such as total amount and period of the bill.
     """
     def __init__(self, amount, period):
         self.amount = amount
         self.period = period
-    
