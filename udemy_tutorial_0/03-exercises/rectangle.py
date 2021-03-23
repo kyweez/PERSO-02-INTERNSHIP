babd7cc879de3408ecb064649f33a608c77ebfe9
kyweez <auboudier.pro@gmail.com>
2021-03-23 08:25:37 +0100

    udemy/exercise-distance-to-point-done

--- udemy_tutorial_0/03-exercises/rectangle.py
+++ udemy_tutorial_0/03-exercises/rectangle.py
@@ -1,8 +1,13 @@
 class Rectangle:
 
-    def __init__(self, width, height):
+    def __init__(self, width, height, x, y):
         self.width = width
         self.height = height
-    
+        self.x = x
+        self.y = y
+
     def area(self):
         return self.width * self.height
+    
+    def distance_to_point(self, x, y):
+        return ((self.x - x)**2 + (self.y - y)**2)**0.5
